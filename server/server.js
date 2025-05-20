76bf0eaf843c11d9389a1b2d8c9608403eb4c6c6
sahsushant <sahsushant34@gmail.com>
2025-05-20 17:16:44 -0500

    Initial backend and frontend setup

---
+++ server/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI || 'your_mongo_connection_string_here';
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+  res.send('BitSpire backend running');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
